fada4a69628db4046afc5c03f799f103a27fde09
daviemakz <d.makuni@live.co.uk>
2020-11-21 17:41:29 +0000

    feat: update docs

--- init.sh
+++
@@ -1,7 +0,0 @@
-## Install docs packages
-yarn install
-
-# Install website packages
-cd website
-yarn install
-cd ..
--- update.sh
+++
@@ -1,9 +0,0 @@
-## Initialise submodules
-./init.sh
-
-# Add latest commit
-git add -A
-git commit -a -m "feat: updating files..."
-
-# Push the repo
-git push
